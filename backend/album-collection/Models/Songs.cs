c13b60a50e4490a4d2a3de9c7ccec6f947aed5a5
Kashmellow <ejodlowsky@pm.me>
2020-03-11 13:53:29 -0400

    added properties and constructors for Songs model

--- backend/album-collection/Models/Songs.cs
+++ backend/album-collection/Models/Songs.cs
@@ -7,5 +7,20 @@ namespace album_collection.Models
 {
     public class Songs
     {
+        public string Title { get; set; }
+        public string Links { get; set; }
+        public string Duration { get; set; }
+
+        public Songs()
+        {
+        }
+
+        public Songs(string title, string links, string duration)
+        {
+            Title = title;
+            Links = links;
+            Duration = duration;
+
+        }
     }
 }
