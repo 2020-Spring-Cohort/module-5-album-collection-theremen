73ff0d98c6c7b58bb090e61815316725c31b1da4
Kashmellow <ejodlowsky@pm.me>
2020-03-11 13:48:10 -0400

    created properties and constructors for Album model

--- backend/album-collection/Models/Album.cs
+++ backend/album-collection/Models/Album.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace album_collection.Models
 {
     public class Album
     {
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Songs { get; set; }
+        public string RecordLabel { get; set; }
+
+        public Album()
+        {
+        }
+
+        public Album(string title, string image, string songs, string recordLabel)
+        {
+            Title = title;
+            Image = image;
+            Songs = songs;
+            RecordLabel = recordLabel;
+
+        }
     }
+
+    
 }
