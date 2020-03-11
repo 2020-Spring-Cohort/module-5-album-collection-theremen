89f14a530e33e1d73d7a431fec4cf4dbedc8103e
Kashmellow <ejodlowsky@pm.me>
2020-03-11 13:51:25 -0400

    added properties and constructors for Artist model

--- backend/album-collection/Models/Artist.cs
+++ backend/album-collection/Models/Artist.cs
@@ -7,5 +7,22 @@ namespace album_collection.Models
 {
     public class Artist
     {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Albums { get; set; }
+        public string Hometown { get; set; }
+
+        public Artist()
+        {
+        }
+
+        public Artist(string name, string image, string albums, string hometown)
+        {
+            Name = name;
+            Image = image;
+            Albums = albums;
+            Hometown = hometown;
+
+        }
     }
 }
