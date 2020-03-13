0af9bacf7c37648eba5e1c5d4f6cca3aee90e615
Eric Drexler <ericdrxlr@gmail.com>
2020-03-13 10:25:28 -0400

    added song id

--- backend/album-collection/Models/Songs.cs
+++ backend/album-collection/Models/Songs.cs
@@ -7,6 +7,7 @@ namespace album_collection.Models
 {
     public class Songs
     {
+        public int SongId {get; set;}
         public string SongTitle { get; set; }
         public string Links { get; set; }
         public string Duration { get; set; }
