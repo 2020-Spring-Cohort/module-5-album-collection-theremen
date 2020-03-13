1f5e772ac8b7cf9b59b4df05d384da6a6aa688fc
anmesk <an_mesk@live.com>
2020-03-13 15:02:35 -0400

    Added repositories for albums, artists and songs

---
+++ backend/album-collection/Repositories/ArtistRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using album_collection.Models;
+
+namespace album_collection.Repositories
+{
+    public class ArtistRepository : Repository<Artist>
+    {
+        public ArtistRepository(AlbumCollectionContext context) : base(context)
+        {
+
+        }
+    }
+}
