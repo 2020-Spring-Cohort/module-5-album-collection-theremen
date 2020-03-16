090d9fb4cef07fd94f9205157f2db4d468f16d9a
Eric Drexler <ericdrxlr@gmail.com>
2020-03-16 10:25:42 -0400

    added proxy package and some code in context

--- backend/album-collection/Models/AlbumCollectionContext.cs
+++ backend/album-collection/Models/AlbumCollectionContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using album_collection.Models;
+
 
 namespace album_collection.Models
 {
@@ -14,5 +16,13 @@ namespace album_collection.Models
         public DbSet<Album> Albums { get; set; }
         public DbSet<Artist> Artists { get; set; }
         public DbSet<Songs> Songs { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=album_collection;Trusted_Connection=True;";
+            optionsBuilder.UseSqlServer(connectionString)
+                .UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
     }
+    
 }
