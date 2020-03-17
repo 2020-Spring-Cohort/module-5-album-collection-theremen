0f2ab18c7e7faf51e18d704bc3ec9452898c59f5
Eric Drexler <ericdrxlr@gmail.com>
2020-03-17 10:22:31 -0400

    deleted unnecessary things in here

--- backend/album-collection/Controllers/SongsController.cs
+++ backend/album-collection/Controllers/SongsController.cs
@@ -9,29 +9,29 @@ namespace album_collection.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SongsController : ControllerBase
+    public class SongController : ControllerBase
     {
-        // GET: api/Songs
+        // GET: api/Song
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Songs/5
+        // GET: api/Song/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Songs
+        // POST: api/Song
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Songs/5
+        // PUT: api/Song/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
--- backend/album-collection/Models/Songs.cs
+++ backend/album-collection/Models/Songs.cs
@@ -5,18 +5,18 @@ using System.Threading.Tasks;
 
 namespace album_collection.Models
 {
-    public class Songs
+    public class Song
     {
         public int SongId {get; set;}
         public string SongTitle { get; set; }
         public string Links { get; set; }
         public string Duration { get; set; }
 
-        public Songs()
+        public Song()
         {
         }
 
-        public Songs(int id, string songTitle, string links, string duration)
+        public Song(int id, string songTitle, string links, string duration)
         {
             SongId = id;
             SongTitle = songTitle;
