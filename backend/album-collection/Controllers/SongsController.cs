9d53daf8e9290b626e5af6732c7412f69a671af9
Eric Drexler <ericdrxlr@gmail.com>
2020-03-17 10:00:11 -0400

    deleted name from route expression

--- backend/album-collection/Controllers/SongsController.cs
+++ backend/album-collection/Controllers/SongsController.cs
@@ -19,7 +19,7 @@ namespace album_collection.Controllers
         }
 
         // GET: api/Songs/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
