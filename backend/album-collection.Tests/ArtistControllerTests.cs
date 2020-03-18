66e8577cf343e08d536be0ba64aff1a17987510a
Eric Drexler <ericdrxlr@gmail.com>
2020-03-18 10:19:26 -0400

    josh made test run for getall action

--- backend/album-collection.Tests/ArtistControllerTests.cs
+++ backend/album-collection.Tests/ArtistControllerTests.cs
@@ -5,6 +5,7 @@ using album_collection.Models;
 using album_collection.Repositories;
 using Xunit;
 using NSubstitute;
+using System.Linq;
 
 namespace album_collection.Tests
 {
@@ -24,13 +25,17 @@ namespace album_collection.Tests
             // arrange
             var expectedArtists = new List<Artist>()
             {
-              new Artist(1, "Mumford & Sons", "mumfordandsons.jpg", "London")
+              new Artist(1, "Mumford & Sons", "mumfordandsons.jpg", "London"),
+              new Artist(2, "Test Artist", "testartist.jpg", "test")
             };
             //artistList
-
+            artistMockRepo.GetAll().Returns(expectedArtists);
             // act
+            var result = testController.Get();
+            var countOfArtists = result.Count();
 
             // assert
+            Assert.Equal(2, countOfArtists);
 
         }
     }
