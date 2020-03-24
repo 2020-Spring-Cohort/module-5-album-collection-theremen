a15e4ffd9b1c734ffc54e3e292557ff5ece82095
anmesk <an_mesk@live.com>
2020-03-24 10:23:21 -0400

    Wrote Song Count return test for Get method

--- backend/album-collection.Tests/SongControllerTests.cs
+++ backend/album-collection.Tests/SongControllerTests.cs
@@ -19,5 +19,26 @@ namespace album_collection.Tests
             songMockRepo = Substitute.For<IRepository<Song>>();
             testController = new SongController(songMockRepo);
         }
+
+        [Fact]
+        public void Get_Returns_Count_Of_Songs()
+        {
+            // arrange
+            var expectedArtists = new List<Song>()
+            {
+              new Song(1, "Sigh No More", "link.html", "3:30"),
+              new Song(2, "Test Song", "testlink.html", "test")
+            };
+
+            //songList
+            songMockRepo.GetAll().Returns(expectedSongs);
+
+            // act
+            var result = testController.Get();
+            var countOfArtists = result.Count();
+
+            // assert
+            Assert.Equal(2, countOfArtists);
+        }
     }
 }
