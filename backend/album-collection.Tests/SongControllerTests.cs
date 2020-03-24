d7747290a94fa0ffcae8f71048613439dc99a577
anmesk <an_mesk@live.com>
2020-03-24 10:26:28 -0400

    Wrote test to ensure that GetById returns chosen Song

--- backend/album-collection.Tests/SongControllerTests.cs
+++ backend/album-collection.Tests/SongControllerTests.cs
@@ -24,7 +24,7 @@ namespace album_collection.Tests
         public void Get_Returns_Count_Of_Songs()
         {
             // arrange
-            var expectedArtists = new List<Song>()
+            var expectedSongs = new List<Song>()
             {
               new Song(1, "Sigh No More", "link.html", "3:30"),
               new Song(2, "Test Song", "testlink.html", "test")
@@ -39,6 +39,26 @@ namespace album_collection.Tests
 
             // assert
             Assert.Equal(2, countOfArtists);
+        }
+
+        [Fact]
+        public void GetById_Should_Return_Chosen_Song()
+        {
+            //arrange
+            var id = 2;
+            var firstSong = new Song(1, "Sigh No More", "link.html", "3:30");
+            var secondSong = new Song(2, "Test Song", "testsong.jpg", "test");
+            var expectedSongs = new List<Song>();
+            expectedSongs.Add(firstSong);
+            expectedSongs.Add(secondSong);
+
+            songMockRepo.GetById(id).Returns(secondSong);
+
+            //act
+            var result = testController.Get(id);
+
+            //assert
+            Assert.Equal(secondSong, result);
         }
     }
 }
