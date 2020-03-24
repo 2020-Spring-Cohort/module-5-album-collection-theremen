69cc0f0daf3f5d642ee74c1d9aedfb22add53230
Eric Drexler <ericdrxlr@gmail.com>
2020-03-24 10:22:16 -0400

    edited view album page

--- frontend/src/js/components/ViewAlbums.js
+++ frontend/src/js/components/ViewAlbums.js
@@ -1,12 +1,13 @@
 export default function ViewAlbums(artist) {
     return `
+    <h3>${artist.name}</h3>
     <ul id="view-albums">
         ${artist.albums.map(album => {
             return `
             <li class="album">
-                <div class="album__name">${album.title}</div>
-                ${album.image}
-                ${album.recordLabel}
+                <h4 class="album__name">${album.title}</h4>
+                <div>${album.image}</div>
+                <div>${album.recordLabel}</div>
                 <button class="edit-album__submit">Edit</button>
                 <button class="delete-album__submit">Delete</button>
                 <input class="album__id" type="hidden" value="${album.albumId}">
