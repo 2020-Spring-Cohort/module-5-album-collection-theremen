904d2b85d252586a807b75b3528d8218d44218af
anmesk <an_mesk@live.com>
2020-03-24 10:02:13 -0400

    Started writing AlbumControllerTests, prepared mock tests accordingly

---
+++ backend/album-collection.Tests/AlbumControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using album_collection.Controllers;
+using album_collection.Models;
+using album_collection.Repositories;
+using Xunit;
+using NSubstitute;
+using System.Linq;
+
+namespace album_collection.Tests
+{
+    class AlbumControllerTests
+    {
+        AlbumController testController;
+        IRepository<Album> albumMockRepo;
+
+        public AlbumControllerTests()
+        {
+            albumMockRepo = Substitute.For<IRepository<Album>>();
+            testController = new AlbumController(albumMockRepo);
+        }
+    }
+}
